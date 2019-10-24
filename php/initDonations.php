dd52bc36fd9eaf8b25d0d92688a3106f4f6e729a
tms326 <31251865+tms326@users.noreply.github.com>
2019-10-24 11:39:28 -0400

    Delete initDonations.php

--- php/initDonations.php
+++
@@ -1,6 +0,0 @@
-Initiative: clean up neighbor's yard - Donation Type: volunteer - Amount Donated: 1<br>Initiative: cancer research - Donation Type: money - Amount Donated: 100<br>Initiative: Ball State Food drive - Donation Type: items - Amount Donated: 10<br>
-
-
-
-
-
